1acf410f6742bf9a3b6d8e202b5914726d6d67ae
Joshuaweg <joshua.weg.95@gmail.com>
2023-02-22 19:32:02 -0800

    products page is now routed. still need the model so the template can read in the content

--- products/views.py
+++ products/views.py
@@ -7,6 +7,6 @@ def product_list(request):  # Rename is necessary
     # Store all products in our database
     all_products = Product.objects.all()
 
-    return render(request, 'product_list.html', {'products': all_products})
+    return render(request, 'products/product_list.html', {'products': all_products})
 
     # return render(request, 'Name of html file goes here product.html', {'products': all_products})
