39ba501ac3737f9bb9e8b3b1b9b9ec2400c18738
troykerim <troykerim@gmail.com>
2023-04-05 20:29:39 -0700

    Adding urls and views for email app

--- email_confirmation/views.py
+++ email_confirmation/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def email_confirmation(request):  # Rename is necessary
+   
+    pass
+
+    #return render(request, '') # html template goes here
