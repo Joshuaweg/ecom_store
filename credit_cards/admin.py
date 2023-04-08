1b7b7c59ea9ba578da6494cf4254632987af1e15
Denshire <vyacheslav.shaerman.068@my.csun.edu>
2023-04-08 13:27:15 -0700

    Create credit cards model and user-credit card table model, and add both to admin panel

--- credit_cards/admin.py
+++ credit_cards/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from credit_cards import models
 
 # Register your models here.
+
+
+class CreditCardAdmin(admin.ModelAdmin):
+    list_display = ("credit_card_number",)
+
+
+class UserCreditCardAdmin(admin.ModelAdmin):
+    list_display = ("user_id",)
+
+
+admin.site.register(models.CreditCard, CreditCardAdmin)
+admin.site.register(models.UserCreditCard, UserCreditCardAdmin)
--- credit_cards/models.py
+++ credit_cards/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class CreditCard(models.Model):
+    credit_card_number = models.IntegerField(max_length=16)
+    expiration_date = models.CharField(max_length=5)
+    cvv = models.IntegerField(max_length=3)
+
+
+class UserCreditCard(models.Model):
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    credit_card = models.ForeignKey(CreditCard, on_delete=models.CASCADE)
