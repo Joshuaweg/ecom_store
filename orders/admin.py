e8125b0ff9e9b7c76ef2f2080bdc5381252cfd85
Denshire <vyacheslav.shaerman.068@my.csun.edu>
2023-04-12 20:33:37 -0700

    Created cart model and cart-product table

--- orders/admin.py
+++ orders/admin.py
@@ -7,4 +7,15 @@ from . import models
 class OrderAdmin(admin.ModelAdmin):
     list_display = ("id",)
 
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+
+class CartTableAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+
 admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.Cart, CartAdmin)
+admin.site.register(models.CartProductTable, CartTableAdmin)
