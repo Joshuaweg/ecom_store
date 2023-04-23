47d2a9c730df3bc4e303404d9290e7f4b817aebb
troykerim <troykerim@gmail.com>
2023-04-22 20:36:31 -0700

    Adding cart funcitionality to multiple pages

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from orders.models import CartProductTable, Cart # If not here, then CartProductTable gives error
+
 
 # Create your views here.
 def order(request):  # Rename is necessary
-    cart=CartProductTable.objects.get(pk=request.GET.get("id"))
-    return render(request, 'orders/index.html')
-def AddtoCart(request):  # Rename is necessary
-    cart=CartProductTable.objects.get(pk=request.GET.get("id"))
+    if request.user.is_authenticated :
+        cart=CartProductTable.objects.get(pk=request.GET.get("id"))
+        cartList=CartProductTable.objects.get(associated_cart=cart.id) 
+        return render(request, 'orders/index.html', {"cart":cartList}) # Does this datetime?
+    else:
+         return render(request, 'orders/index.html')
+def AddtoCart(request):  
+        # This Funcsiton is For Joshua to work on
+        cart=CartProductTable.objects.get(pk=request.GET.get("id"))
+        cartList=CartProductTable.objects.get(associated_cart=cart.id) # Does this need to be here?
+        # Does this need statement?
+
+# ORIGINAL CODE:
+# def order(request):  # Rename is necessary
+#    cart=CartProductTable.objects.get(pk=request.GET.get("id"))
+#    return render(request, 'orders/index.html')
+# def AddtoCart(request):  # Rename is necessary
+#    cart=CartProductTable.objects.get(pk=request.GET.get("id")) 
+
+#    if request.user.is_authenticated :
+#        cart = Cart.objects.get(associated_user=request.user.id)
+#        cartList=CartProductTable.objects.get(associated_cart=cart.id)
+#        return render(request,'home/index.html',{'today':datetime.today(),"cart":cartList}) #add 'cart':cart
+#    else:
+#        return render(request,'home/index.html',{'today':datetime.today()})    
     
--- products/views.py
+++ products/views.py
@@ -1,13 +1,51 @@
 from django.shortcuts import render
 from .models import Product
 
+from orders.models import CartProductTable,Cart # to add 'cart': cart 
+
+
 # Create your views here.
 #this is th Product List View
 def product_list(request):  # Rename is necessary
     all_products = Product.objects.all()
-    return render(request, 'products/product_list.html', {'products': all_products}) #add 'cart':cart
+    
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(associated_user=request.user.id)
+        cartList=CartProductTable.objects.get(associated_cart=cart.id)
+        
+        return render(request, 'products/product_list.html', {'products': all_products, 'cart': cart})
+    else:
+        return render(request, 'products/product_list.html', {'products': all_products}) #add 'cart':cart
+    
 #this is the product page View
 def product_page(request):
-    print(request.GET)
+    all_products = Product.objects.all()
+    print(request.GET)  # previous commit
     product = Product.objects.get(pk=request.GET.get("id"))
-    return render(request,'products/product_page.html',{'product':product})#add 'cart':cart
+
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(associated_user=request.user.id)
+        cartList=CartProductTable.objects.get(associated_cart=cart.id)
+
+        return render(request,'products/product_page.html',{'product':product ,'cart': cart})
+    else:
+        return render(request,'products/product_page.html',{'product':product})#add 'cart':cart
+
+# ORIGINAL CODE BEFORE THE IF/ELSE BLOCK
+#def product_list(request):  # Rename is necessary
+#    all_products = Product.objects.all()
+#    return render(request, 'products/product_list.html', {'products': all_products}) #add 'cart':cart
+##this is the product page View
+#def product_page(request):
+#    print(request.GET)
+#    product = Product.objects.get(pk=request.GET.get("id"))
+#    return render(request,'products/product_page.html',{'product':product})#add 'cart':cart
+
+
+
+#    if request.user.is_authenticated :
+#        cart = Cart.objects.get(associated_user=request.user.id)
+#        cartList=CartProductTable.objects.get(associated_cart=cart.id)
+#        return render(request,'home/index.html',{'today':datetime.today(),"cart":cartList}) #add 'cart':cart
+#    else:
+#        return render(request,'home/index.html',{'today':datetime.today()})
